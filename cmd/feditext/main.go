bb766d7fa24910d4f8ca073eda7e2f1932eedf53
KushBlazingJudah <59340248+KushBlazingJudah@users.noreply.github.com>
2022-05-11 16:30:17 -0300

    cmd/feditext: helpful message if no config

--- cmd/feditext/main.go
+++ cmd/feditext/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/KushBlazingJudah/feditext"
 	"github.com/KushBlazingJudah/feditext/config"
 )
 
 func main() {
-	if err := config.Load("./feditext.config"); err != nil {
+	if err := config.Load("./feditext.config"); err != nil && errors.Is(err, os.ErrNotExist) {
+		fmt.Printf("Feditext was unable read it's configuration file, ./feditext.config!"+
+			"Error: %s\n\n"+
+			"Have you created the file? If not:\n"+
+			"- copy doc/config.example to ./feditext.config"+
+			"- read its contents carefully and modify as you see fit"+
+			"- restart Feditext", err)
+		os.Exit(1)
+	} else if err != nil {
 		panic(err)
 	}
 
